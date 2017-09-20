6607f07271b105873ee7cae21b50ffa94f91900c
Ishank Singh <ishdemon10@gmail.com>
2017-09-20 15:11:09 +0530

    Add files via upload

---
+++ disabler.ps1
@@ -0,0 +1,33 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+$Warning = "
+
+
+██╗███████╗██╗  ██╗██████╗ ███████╗███╗   ███╗ ██████╗ ███╗   ██╗
+██║██╔════╝██║  ██║██╔══██╗██╔════╝████╗ ████║██╔═══██╗████╗  ██║
+██║███████╗███████║██║  ██║█████╗  ██╔████╔██║██║   ██║██╔██╗ ██║
+██║╚════██║██╔══██║██║  ██║██╔══╝  ██║╚██╔╝██║██║   ██║██║╚██╗██║
+██║███████║██║  ██║██████╔╝███████╗██║ ╚═╝ ██║╚██████╔╝██║ ╚████║
+╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+                                                                 
+
+"
+foreach ($line in $Warning -split "`n")
+{
+    foreach ($char in $line.tochararray())
+    {
+        if ($([int]$char) -le 9580 -and  $([int]$char) -ge 9552)
+        {
+            Write-host -fore cyan $char -NoNewline
+        }
+        else
+        {
+            write-host -fore red $char -NoNewline
+        }
+    }
+    write-host ""
+}
+(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+import-module PsGet
+install-module PsUrl
+Install-Module PsHosts
+Add-HostEntry 
